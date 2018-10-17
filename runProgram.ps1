36c04e0614019ae1bdff1b252a53ac804948da05
Anthony Williams <anthony.williams.cs@gmail.com>
2018-10-16 22:17:04 -0600

    Update run script

--- runProgram.ps1
+++ runProgram.ps1
@@ -1,5 +1,8 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
 $artifactName = 'javafun-1.0-SNAPSHOT.jar'
 $mainClass = 'App'
 $mainPackage = 'com.williams.anthony'
 
+Start-Process mvn -NoNewWindow -Wait -ArgumentList "clean", "install"
 Start-Process java -NoNewWindow -Wait -ArgumentList "-cp", ".\target\$artifactName", "$mainPackage.$mainClass"
